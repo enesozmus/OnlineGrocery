42a0e7a0e841182b79a081e9f3c724c5384a309b
enesozmus <ensozmus@gmail.com>
2025-08-16 18:07:09 +0300

    Initial Commit

---
+++ OnlineGrocery/OnlineGroceryApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineGroceryApp.swift
+//  OnlineGrocery
+//
+//  Created by enesozmus on 16.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct OnlineGroceryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
